54626b2c9b3fc01b211978636b74647c4a43d5a1
edijun <edijun.design@gmail.com>
2021-05-24 10:28:25 +0700

    spring initializr

---
+++ src/main/java/id/edijun/interpolrednoticescraper/InterpolRedNoticeScraperApplication.java
@@ -0,0 +1,13 @@
+package id.edijun.interpolrednoticescraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterpolRedNoticeScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterpolRedNoticeScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/edijun/interpolrednoticescraper/InterpolRedNoticeScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package id.edijun.interpolrednoticescraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterpolRedNoticeScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
